4ffeb0bb3cc3a16e63356f327da605b54ae35e85
denis-fominykh <fominykhdenis@gmail.com>
2019-11-26 18:07:21 +0200

    updating the root component and root module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ModelComponent } from './model/model.component';
-import { StoreComponent } from './store/store.component';
-import { AdminComponent } from './admin/admin.component';
+import { StoreModule } from './store/store.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ModelComponent,
-    StoreComponent,
-    AdminComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
+    StoreModule,
     AppRoutingModule
   ],
   providers: [],
