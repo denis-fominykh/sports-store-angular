44d996bfb1c2b5c49dc8096f71f11f1bd86ed612
denis-fominykh <fominykhdenis@gmail.com>
2019-11-26 17:36:20 +0200

    creating the store component and template

--- src/app/store/store.component.ts
+++ src/app/store/store.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Product } from './../model/product.model';
+import { ProductRepository } from './../model/product.repository';
 
 @Component({
   selector: 'app-store',
   templateUrl: './store.component.html',
   styleUrls: ['./store.component.scss']
 })
-export class StoreComponent implements OnInit {
+export class StoreComponent {
 
-  constructor() { }
+  constructor(private repository: ProductRepository) { }
 
-  ngOnInit() {
+  get products(): Product[] {
+    return this.repository.getProducts();
   }
 
+  get categories(): string[] {
+    return this.repository.getCategories();
+  }
 }
