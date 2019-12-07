c7677258e6887fd2c7882189e03e5b84df6f4afb
denis-fominykh <fominykhdenis@gmail.com>
2019-12-08 00:42:52 +0200

    using routes’ constant for routing configuration

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,15 +6,15 @@ import { CheckoutComponent } from './store/checkout.component';
 import { CartDetailComponent } from './store/cartDetail.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'store', component: StoreComponent },
+  { path: 'cart', component: CartDetailComponent },
+  { path: 'checkout', component: CheckoutComponent },
+  { path: '**', redirectTo: '/store' }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot([
-    { path: 'store', component: StoreComponent },
-    { path: 'cart', component: CartDetailComponent },
-    { path: 'checkout', component: CheckoutComponent },
-    { path: '**', redirectTo: '/store' }
-  ])],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
