fa041674a504ab64de56245b1991de2c6e02265f
denis-fominykh <fominykhdenis@gmail.com>
2019-12-09 23:57:58 +0200

    navigating through the application

--- src/app/store/store.component.ts
+++ src/app/store/store.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Product } from './../model/product.model';
 import { ProductRepository } from './../model/product.repository';
 import { Cart } from './../model/cart.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-store',
@@ -15,7 +16,8 @@ export class StoreComponent {
 
   constructor(
     private repository: ProductRepository,
-    private cart: Cart
+    private cart: Cart,
+    private router: Router
   ) { }
 
   get products(): Product[] {
@@ -49,5 +51,6 @@ export class StoreComponent {
 
   addProductToCart(product: Product) {
     this.cart.addLine(product);
+    this.router.navigateByUrl('/cart');
   }
 }
