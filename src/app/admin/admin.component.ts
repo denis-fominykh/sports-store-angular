dd91e76e122009223ff326df222e17a0448abdff
denis-fominykh <fominykhdenis@gmail.com>
2020-02-05 21:08:26 +0200

    preparing the common content and the feature module

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,8 +1,20 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from './../model/auth.service';
 
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
   styleUrls: ['./admin.component.scss']
 })
-export class AdminComponent {}
+export class AdminComponent {
+  constructor(
+    private auth: AuthService,
+    private router: Router
+  ) { }
+
+  logout() {
+    this.auth.clear();
+    this.router.navigateByUrl('/');
+  }
+}
